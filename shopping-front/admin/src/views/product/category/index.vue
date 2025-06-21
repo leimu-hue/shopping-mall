405d8d3961a48298e26713830dfa947cfe97de70
huanghaiqin <2627492139@qq.com>
2025-06-21 17:19:25 +0800

    1、提交前端分类的代码

--- shopping-front/admin/src/views/product/category/index.vue
+++ shopping-front/admin/src/views/product/category/index.vue
@@ -1,37 +1,77 @@
 <template>
-    <div>
-        <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
-    </div>
+  <div>
+    <el-tree :data="data" node-key="catId" :props="defaultProps" @node-click="handleNodeClick" :expand-on-click-node="false">
+        <span class="custom-tree-node" slot-scope="{ node, data }">
+            <span>{{ node.label }}</span>
+            <span>
+            <el-button v-if="node.level <= 2"
+                type="text"
+                size="mini"
+                @click="() => append(data)">
+                Append
+            </el-button>
+            <el-button v-if="node.childNodes.length == 0"
+                type="text"
+                size="mini"
+                @click="() => remove(node, data)">
+                Delete
+            </el-button>
+            </span>
+        </span>
+    </el-tree>
+  </div>
 </template>
 
 <script>
-// import {getMenuList} from "@/api/product/category/index"
+import { getMenuList } from "@/api/product/category/index";
 
 export default {
-    data() {
-        return {
-        data: [],
-            defaultProps: {
-                children: 'children',
-                label: 'label'
-            }
-        };
+  data() {
+    return {
+      data: [],
+      defaultProps: {
+        children: "children",
+        label: "name",
+      },
+      isGetData: false,
+      errorMsg: "",
+    };
+  },
+  created() {
+    this.getMenu();
+  },
+  methods: {
+    handleNodeClick(data) {
+      console.log(data);
     },
-    created() {
-        // this.getMenu();
+    getMenu() {
+      getMenuList({}).then((response) => {
+        if (response.code == 0) {
+          this.isGetData = true;
+          this.data = response.data;
+        } else {
+          this.isGetData = false;
+          this.errorMsg = response.success;
+        }
+      });
     },
-    methods: {
-        handleNodeClick(data) {
-            console.log(data);
-        },
-        // getMenu() {
-        //     getMenuList({}).then((response) => {
-        //         console.log(response);
-        //     })
-        // }
+    append(data) {
+        console.log(data);
+        const newChild = { name: 'testtest', children: [] };
+        if (!data.children) {
+          this.$set(data, 'children', []);
+        }
+        data.children.push(newChild);
+    },
+    remove(node, data) {
+        console.log(data)
+        const parent = node.parent;
+        const children = parent.data.children || parent.data;
+        const index = children.findIndex(d => d.catId === data.catId);
+        children.splice(index, 1);
     }
+  },
 };
 </script>
 
-<style>
-</style>
+<style></style>
