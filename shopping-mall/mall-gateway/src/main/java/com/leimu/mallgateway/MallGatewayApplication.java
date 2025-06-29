45bf94f0b7ed3a4c4826c594d94e4949609b1a9f
leimu-hue <2627492139@qq.com>
2025-06-29 16:37:42 +0800

    1、提交若依vue3版本

--- shopping-mall/mall-gateway/src/main/java/com/leimu/mallgateway/MallGatewayApplication.java
+++ shopping-mall/mall-gateway/src/main/java/com/leimu/mallgateway/MallGatewayApplication.java
@@ -10,6 +10,7 @@ import reactor.core.publisher.Hooks;
 public class MallGatewayApplication {
 
     public static void main(String[] args) {
+        // 开启上下文，允许MCD可以在线程中传输
         Hooks.enableAutomaticContextPropagation();
         SpringApplication.run(MallGatewayApplication.class, args);
     }
